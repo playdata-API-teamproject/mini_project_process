6622f97efe4fda98103ec32a60c9237a42157606
jeeseungbae <sdgh1442@gmail.com>
2020-11-11 01:32:14 +0900

    최신

--- netflix데이터가져오기/netflix_frame_module.py
+++ netflix데이터가져오기/netflix_frame_module.py
@@ -1,3 +1,4 @@
+%%writefile C:\myPyCode\팀프로젝트\netflix_frame_module.py
 import pandas as pd
 
 df = pd.read_csv("netflix_titles.csv",encoding="latin1")
@@ -7,4 +8,11 @@ def netflix_date_selection(select_col,Name) :
     #select.sort_values(by = ['release_year','date_added']) 날짜별로 구분하기
     selection = select[select[select_col] == Name].sort_values(by = ['release_year','date_added'])
     return selection
-    
+
+def netflix_data_findword(position,Name) :
+    find_line = select.loc[select[position].str.contains(Name)].sort_values(by = ['release_year','date_added'])
+    return find_line
+#     df['email_startswith'] = df['email'].str.startswith(pat='h')     # 좌측값이 입력패턴과 일치 여부
+#     df['email_endswith']   = df['email'].str.endswith(pat='com')     # 우측값이 입력패턴과 일치 여부
+#     df['email_contains']   = df['email'].str.contains(pat='kr', regex=False) # 값 중 패턴포함 여부
+#     df['email_match']      = df['email'].str.match(pat='[a-zA-Z@.]') # 입력패턴과 일치 여부
